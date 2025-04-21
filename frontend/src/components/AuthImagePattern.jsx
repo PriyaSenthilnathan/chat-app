b37462707b4bfdc6e56d68d64fe249971d89fa50
priya6804 <spriyadarshini.22cse@kongu.edu>
2025-04-21 19:27:42 +0530

    added

---
+++ frontend/src/components/AuthImagePattern.jsx
@@ -0,0 +1,17 @@
+const AuthImagePattern = ({ title, subtitle }) => {
+  return (
+    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+      <div className="max-w-md text-center">
+        {/* Use a background image */}
+        <div
+          className="w-full h-64 bg-cover bg-center rounded-2xl mb-8"
+          style={{ backgroundImage: "url('https://img.freepik.com/free-vector/dialogue-chat-clouds-speech-bubble-icon-from-lines-triangles-particle-style-design-low-poly-technology-devices-people-communication-concept-blue-background_587448-471.jpg?semt=ais_hybrid')" }} // Replace with the actual path to your image
+        />
+        <h2 className="text-2xl font-bold mb-4">{title}</h2>
+        <p className="text-base-content/60">{subtitle}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AuthImagePattern;
